9f29d6cfdfcfde9e3bd4d316df25267adb1f973b
SanikaC1999 <131801208+SanikaC1999@users.noreply.github.com>
2024-03-02 21:41:59 +0530

    final

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import React, { useState } from "react"
 
-export default function App() {
-  const [count, setCount] = useState(0);
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  return (
-    <div className="App">
-      <h1>Count : {count}</h1>
-      <div className="buttons">
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-      </div>
-    </div>
-  );
+    
+
+
+
+export default function Counter (){
+    const[count,setCount] = useState(0);
+
+    const handleChangeIncremnt = ()=>{
+        setCount((count)=>count+1)
+    }
+
+    const handleChangeDecrement = ()=>{
+        setCount((count)=>count-1)
+    }
+
+    return(
+        <div>
+            <h1>Counter App</h1>
+            <p>Count: {count}</p>
+            <button onClick={handleChangeIncremnt}>Increment</button>
+            <button onClick={handleChangeDecrement}>Decrement</button>
+        </div>
+    )
 }
